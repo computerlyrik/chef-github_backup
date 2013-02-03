36cc90543a8668f93907bcd3ec7bfb01bfea09fc
Christian Fischer <sw-dev@computerlyrik.de>
2013-02-03 18:14:45 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name            "github-backup"
+maintainer       "computerlyrik"
+maintainer_email "chef-cookbooks@computerlyrik.de"
+license          "Apache 2.0"
+description      "Installs/Configures github-backup"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+supports        "ubuntu"
