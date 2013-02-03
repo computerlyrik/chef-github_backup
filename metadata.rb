5eee3f8f49e59a101ee9c4b079b2466450f01391
Christian Fischer <sw-dev@computerlyrik.de>
2013-02-03 21:35:09 +0100

    updated description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name            "github-backup"
 maintainer       "computerlyrik"
 maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
-description      "Installs/Configures github-backup"
+description      "backup your public github repos"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 supports        "ubuntu"
